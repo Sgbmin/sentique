827eb21aa29ab816824763f4cf2ba7e38b587481
Dion <144816218+Sgbmin@users.noreply.github.com>
2026-01-04 01:13:51 +0330

    last

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   extends: ['@nuxt/ui'],
   
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', '@nuxt/ui'],
   
   supabase: {
     redirectOptions: {
