3601610ec761b9a40eb3b96f343a0fed41afd637
Dion <144816218+Sgbmin@users.noreply.github.com>
2026-01-04 11:47:19 +0330

    updating app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,18 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'amber', // Gold
-      secondary: 'emerald', // Jade green  
-      tertiary: 'blue' // Navy blue
+      primary: '#0F766E', 
+      secondary: '#C9A24D',  
+      tertiary: '#0B1C2D' ,
     },
     button: {
       slots: {
-        base: 'transition-all duration-300 hover:scale-105'
+        base: 'transition-all duration-300 hover:scale-105 rounded-lg px-6 py-2 font-medium',
       }
     },
     card: {
       slots: {
-        root: 'overflow-hidden'
+        root: 'overflow-hidden rounded-xl shadow-lg p-4 bg-[#0B1C2D] text-white',
       }
     }
   }
